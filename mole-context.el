0f9a60bd024f7bc042c8b4f5a62044499d08baab
felipe <felipe@fov.space>
2017-12-13 13:16:15 -0500

    [feat] mole-context

---
+++ mole-context.el
@@ -0,0 +1,58 @@
+;;; mole-context.el --- Cacheable alists  -*- lexical-binding: t -*-
+
+;; Copyright (C) 2017 Felipe Ochoa
+
+;; Author: Felipe Ochoa <felipe@fov.space>
+;; URL: https://github.com/felipeochoa/mole/
+;; Created: 13 Dec 2017
+;; Package-Requires: ((emacs "25.2"))
+;; Version: 0.1
+;; Keywords: maint
+
+;; This file is not part of GNU Emacs.
+
+;;; Commentary:
+;;; This file includes utility functions for creating, updating, and
+;;; comparing small key-value stores where the store itself is easily
+;;; cacheable.  These stores are used in the mole packrat parser to
+;;; efficiently cache parse results that may be context dependent.
+
+;;; Code:
+
+(defvar mole-context-compare--default (cons nil (make-symbol "not-found"))
+  "Value used to distinguish not found from nil.")
+
+(defun mole-context-compare (c1 c2)
+  "Return t if C1 and C2 are equivalent contexts."
+  (or (eq c1 c2)
+      (and (= (length c1) (length c2))
+           (let ((res t))
+             (while (and res c1)
+               (setq res (eq (cdar c1)
+                             (cdr (or (assq (caar c1) c2)
+                                      mole-context-compare--default)))
+                     c1 (cdr c1)))
+             res))))
+
+(defsubst mole-context-get (ctx key)
+  "Return the CTX's value for KEY."
+  (cdr (assq key ctx)))
+
+(defun mole-context-set (ctx key val)
+  "Return a new context like CTX, but with KEY set to VAL."
+  (cons
+   (cons key val)
+   (if (eq (caar ctx) key)
+       (cdr ctx)
+     (let ((tail ctx) (i 0) (res ctx))
+       (while (setq i (1+ i) tail (cdr tail))
+         (when (eq key (caar tail))
+           (setq res nil)
+           (dotimes (_ i) (push (pop ctx) res))
+           (setq tail nil
+                 res (nconc (nreverse res) (cdr tail)))))
+       res))))
+
+(provide 'mole-context)
+
+;;; mole-context.el ends here
---
+++ test/mole-context-test.el
@@ -0,0 +1,70 @@
+;;; mole-context-test --- Tests for mole-context  -*- lexical-binding: t -*-
+;; Copyright (C) 2017 Felipe Ochoa
+;;; Commentary:
+;;; Code:
+
+(require 'f)
+
+(load (f-expand "mole-context" (f-parent (f-dirname (f-this-file)))))
+
+(defun mole-context-test-sort (ctx)
+  "Return a copy of CTX that has its entries sorted."
+  (sort (copy-alist ctx) (lambda (p1 p2)
+                           (string< (symbol-name (car p1))
+                                    (symbol-name (car p2))))))
+
+(ert-deftest mole-context-compare-eq ()
+  (let ((c '((a . 1) (b . 2))))
+    (should (mole-context-compare c c))))
+
+(ert-deftest mole-context-compare-equal ()
+  (should (mole-context-compare '((a . 1) (b . 2))
+                                '((a . 1) (b . 2)))))
+
+(ert-deftest mole-context-compare-out-of-order ()
+  (should (mole-context-compare '((a . 1) (b . 2))
+                                '((b . 2) (a . 1)))))
+
+(ert-deftest mole-context-compare-different-length ()
+  (should-not (mole-context-compare '((a . 1) (b . 2))
+                                    '((a . 1) (b . 2) (c . 3)))))
+
+(ert-deftest mole-context-compare-different-keys ()
+  (should-not (mole-context-compare '((a . 1) (b . 2))
+                                    '((a . 1) (c . 2))))
+  (should-not (mole-context-compare '((a . 1) (b . nil))
+                                    '((a . 1) (c . 2)))))
+
+(ert-deftest mole-context-compare-different-value ()
+  (should-not (mole-context-compare '((a . 1) (b . 2))
+                                    '((a . 1) (b . 1))))
+  (should-not (mole-context-compare '((a . 1) (b . 2))
+                                    '((b . 1) (a . 1))))
+  (should-not (mole-context-compare '((a . 1) (b . 2))
+                                    '((a . 2) (b . 2))))
+  (should-not (mole-context-compare '((a . 1) (b . 2))
+                                    '((b . 2) (a . 2)))))
+
+(ert-deftest mole-context-get ()
+  (should (eq (mole-context-get '((a . 1) (b . 2)) 'a) 1))
+  (should (eq (mole-context-get '((b . 2) (a . 1)) 'b) 2))
+  (should (eq (mole-context-get '((a . 1) (b . 2)) 'c) nil)))
+
+(ert-deftest mole-context-set-new ()
+  (let ((ctx '((a . 1) (b . 2))))
+    (should (equal (mole-context-test-sort (mole-context-set ctx 'c 3))
+                   '((a . 1) (b . 2) (c . 3))))
+    (should (equal ctx '((a . 1) (b . 2))))))
+
+(ert-deftest mole-context-set-replace ()
+  (let ((ctx '((a . 1) (b . 2))))
+    (should (equal (mole-context-test-sort (mole-context-set ctx 'b 3))
+                   '((a . 1) (b . 3))))
+    (should (equal ctx '((a . 1) (b . 2))))
+    (should (equal (mole-context-test-sort (mole-context-set ctx 'a 3))
+                   '((a . 3) (b . 2))))
+    (should (equal ctx '((a . 1) (b . 2))))))
+
+(provide 'mole-context-test)
+
+;;; mole-context-test.el ends here
