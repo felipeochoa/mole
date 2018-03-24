1399160c1483d753054a4245f74b2f5612db3026
felipe <felipe@fov.space>
2018-03-24 17:29:16 -0500

    [dev] Re-license as GPLv3

--- test/mole-test.el
+++ test/mole-test.el
@@ -1,5 +1,28 @@
 ;;; mole-tests --- Tests for mole.js  -*- lexical-binding: t -*-
+
+;; Copyright (C) 2018 Felipe Ochoa
+
+;; Author: Felipe Ochoa <felipe@fov.space>
+;; URL: https://github.com/felipeochoa/mole/
+;; Created: 10 Nov 2017
 ;; Package-Requires: ((emacs "25.2") (f "0.19.0"))
+;; Version: 0.1
+;; Keywords: maint
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;; This file is not part of GNU Emacs.
 
 ;;; Commentary:
 
