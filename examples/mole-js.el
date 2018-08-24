464e1b82564d9f1925be59b448449068ffe267f8
Felipe Ochoa <felipe@fov.space>
2018-08-24 10:09:51 -0500

    [js] Use symbol production in mole-js

--- examples/mole-js.el
+++ examples/mole-js.el
@@ -39,21 +39,21 @@
                                 (t 'fail)))
                              categories))
    (assign-operator (or "=" "|=" "^=" "&=" "<<=" ">>=" ">>>=" "+=" "-=" "*=" "/=" "%=" "**="))
-   (reserved (or "class" "enum" "export" "extends" "import" "static" "super"))
-   (keyword (or "break"
-                "case" "catch" "class" "const" "continue"
-                "debugger" "default" "delete" "do"
-                "else" "extends" "export"
-                "false" "finally" "for" "function"
-                "if" "in" "instanceof" "import"
-                "let"
-                "new" "null"
-                "return"
-                "super" "switch"
-                "this" "throw" "true" "try" "typeof"
-                "var" "void"
-                "while" "with"
-                "yield"))
+   (reserved (or `class `enum `export `extends `import `static `super))
+   (keyword (or `break
+                `case `catch `class `const `continue
+                `debugger `default `delete `do
+                `else `extends `export
+                `false `finally `for `function
+                `if `in `instanceof `import
+                `let
+                `new `null
+                `return
+                `super `switch
+                `this `throw `true `try `typeof
+                `var `void
+                `while `with
+                `yield))
    (comment (extern (lambda ()
                       (let ((beg (point)))
                         (if (forward-comment 1)
@@ -128,7 +128,7 @@
    (strict-program use-strict-directive (with-context (use-strict t) (* (! eof) statement)))
    (statement
     (or async-function-stmt
-        (: (! "async")
+        (: (! `async)
            (or (: (or break const-var let-expr continue debugger import labeled-stmt return
                       yield-expr throw expr)
                   auto-semi-insert)
@@ -140,33 +140,33 @@
         (: (error "msg.syntax") any)))
    ;; Level 1 productions
    (use-strict-directive (or "'use strict';" "\"use strict\";"))
-   (break "break" (\? name))
-   (const-var (or "const" "var") (must-match variables "msg.invalid.variables"))
-   (let-expr "let" (must-match variables "msg.invalid.variables"))
-   (continue "continue" (\? name))
-   (debugger "debugger")
-   (import "import"
+   (break `break (\? name))
+   (const-var (or `const `var) (must-match variables "msg.invalid.variables"))
+   (let-expr `let (must-match variables "msg.invalid.variables"))
+   (continue `continue (\? name))
+   (debugger `debugger)
+   (import `import
            (\? (if-context (not-top-level t) (error "msg.mod.import.decl.at.top.level")))
            (or string (: import-clause from-clause)))
    (labeled-stmt (+ name ":") statement)
-   (return "return" (\? (if-context (in-function nil) (error "msg.bad.return")))
+   (return `return (\? (if-context (in-function nil) (error "msg.bad.return")))
            expr)
-   (yield-expr "yield" (\? "*") (\? (if-context (in-function nil) (error "msg.bad.yield")))
+   (yield-expr `yield (\? "*") (\? (if-context (in-function nil) (error "msg.bad.yield")))
                (or (if-context (star-p nil) (error "msg.syntax"))
                    assign-expr))
    ;; TODO: next line needs to be lexical but expr needs to be syntactic
-   (throw (lexical "throw" (* whitespace-no-eol) expr))
+   (throw (lexical `throw (* whitespace-no-eol) expr))
    (expr assign-expr (* "," assign-expr))
-   (if "if" condition statement (\? "else" statement))
-   (switch "switch"
+   (if `if condition statement (\? `else statement))
+   (switch `switch
            (must-match "(" "msg.no.paren.switch") expr (must-match ")" "msg.no.paren.after.switch")
            (must-match "{" "msg.no.brace.switch")
            (* case-node))               ; TODO: error if multiple defaults
-   (while "while" condition statement)
-   (do "do" statement (must-match "while" "msg.no.while.do") condition)
+   (while `while condition statement)
+   (do `do statement (must-match `while "msg.no.while.do") condition)
    (for (must-match "(" "msg.no.paren.for")
-        (or (: (or "var" "let" "const") variables) (= ";") expr)
-        (or (: (or "in" "of") expr)     ; TODO: handle 'in obj' getting eaten by expr above.
+        (or (: (or `var `let `const) variables) (= ";") expr)
+        (or (: (or `in `of) expr)     ; TODO: handle 'in obj' getting eaten by expr above.
                                         ; TODO: check for msg.mult.index error
             (: (must-match ";" "msg.no.semi.for")
                (\? expr)
@@ -174,38 +174,38 @@
                (\? expr)))
         (must-match ")" "msg.no.paren.for.ctrl")
         statement)
-   (try "try" (or (= "{") (error "msg.no.brace.try"))
+   (try `try (or (= "{") (error "msg.no.brace.try"))
         statement
         (or finally (: catch (\? finally)) (error "msg.try.no.catchfinally")))
-   (with "with"
+   (with `with
          (or (if-context (use-strict t) "msg.no.with.strict")
              (: (must-match "(" "msg.no.paren.with") expr (must-match ")" "msg.no.paren.after.with")
                 statement)))
    (block "{" (* statement) (must-match "}" "msg.no.brace.block"))
-   (class-stmt "class" (must-match name "msg.unnamed.class.stmt") class)
-   (function-stmt "function"
+   (class-stmt `class (must-match name "msg.unnamed.class.stmt") class)
+   (function-stmt `function
                   (or (: "*" (must-match name "msg.unnamed.function.stmt")
                          (must-match "(" "msg.no.paren.params")
                          (with-context (star-p t) function))
                       (: (must-match name "msg.unnamed.function.stmt")
                          (must-match "(" "msg.no.paren.params")
                          (with-context (star-p nil) function))))
-   (async-function-stmt "async" "function"
+   (async-function-stmt `async `function
                         (or (: "*" (must-match name "msg.unnamed.function.stmt")
                                (must-match "(" "msg.no.paren.params")
                                (with-context (async-p t) (with-context (star-p t) function)))
                             (: (must-match name "msg.unnamed.function.stmt")
                                (must-match "(" "msg.no.paren.params")
                                (with-context (async-p t) (with-context (star-p nil) function)))))
-   (export "export"
+   (export `export
            (\? (if-context (not-top-level t) (error "msg.mod.export.decl.at.top.level")))
            (or class-stmt async-function-stmt function-stmt
                (: (or (: "*" from-clause)
                       (: "{" export-bindings (\? from-clause))
-                      (: "default" (or class-stmt class-expr async-function-stmt async-function-expr
+                      (: `default (or class-stmt class-expr async-function-stmt async-function-expr
                                        function-stmt function-expr
                                        (: (or name expr) auto-semi-insert)))
-                      (: (or "var" "const" "let") variables)
+                      (: (or `var `const `let) variables)
                       name expr)
                   auto-semi-insert)))
    ;; level 2
@@ -217,21 +217,21 @@
                                                     (: "{" export-bindings)
                                                     (error "msg.syntax"))))
                       (error "msg.mode.declaration.after.import")))
-   (from-clause "from" (or string (: "this" "module")))
+   (from-clause `from (or string (: `this `module)))
    (assign-expr (or yield-expr async-arrow arrow
                     (: cond-expr (\? assign-operator assign-expr))))
    (condition (must-match "(" "msg.no.paren.cond") expr (must-match ")" "msg.no.paren.after.cond"))
-   (case-node (or (: "case" expr (must-match ":" "msg.no.colon.case"))
-                  (: "default" (must-match ":" "msg.no.colo.case")))
-              (* (! (or "case" "default" eof) statement)))
-   (catch "catch" (must-match "(" "msg.no.paren.catch")
+   (case-node (or (: `case expr (must-match ":" "msg.no.colon.case"))
+                  (: `default (must-match ":" "msg.no.colo.case")))
+              (* (! (or `case `default eof) statement)))
+   (catch `catch (must-match "(" "msg.no.paren.catch")
           (or (: (char "\{\[") destruct-primary-expr)
               (must-match name "msg.bad.catchcond"))
           (must-match ")" "msg.bad.catchcond")
           (must-match "{" "msg.no.brace.catchblock")
           (* statement)
           (must-match "}" "msg.no.brace.after.body"))
-   (finally "finally" statement)
+   (finally `finally statement)
    (function (with-context (in-function t)
                            (or (if-context (use-strict t) strict-function-params function-body)
                                (if-context (use-strict nil)
@@ -239,15 +239,15 @@
                                                (: strict-function-params function-body-with-strict))))))
    ;; TODO: allow customizing error message for imports
    (export-bindings export-binding (* "," export-binding) (must-match "}" "msg.mod.rc.after.export.spec.list"))
-   (export-binding (or (: identifier-name "as" (or "default" name))
+   (export-binding (or (: identifier-name `as (or `default name))
                        name
                        (: identifier-name (error "msg.mod.as.after.reserved.word"))))
    ;; level 3
-   (async-arrow "async" (or name (: "(" function-params)) "=>"
+   (async-arrow `async (or name (: "(" function-params)) "=>"
                 (or (: (= "{") (with-context (async-p t) statement)) expr))
    (arrow (or name (: "\(" function-params)) "=>"
           (or (: (= "{") (with-context (async-p nil) statement)) expr))
-   (namespace-import (or (: "as" name)))
+   (namespace-import (or (: `as name)))
    (variable (or (: (= (char "\{\[")) destruct-primary-expr
                     (must-match (: "=" assign-expr) "msg.destruct.assign.no.init"))
                  (: (must-match name '"msg.bad.var") (\? "=" (must-match assign-expr '"msg.syntax")))))
@@ -285,13 +285,13 @@
    (bit-xor-expr bit-and-expr (\? "^" bit-xor-expr))
    (bit-and-expr eq-expr (\? "&" bit-and-expr))
    (eq-expr rel-expr (\? (or "!==" "!=" "===" "==") rel-expr))
-   (rel-expr shift-expr (\? (or "instanceof" "in" "<=" "=>" "<" ">") shift-expr))
+   (rel-expr shift-expr (\? (or `instanceof `in "<=" "=>" "<" ">") shift-expr))
    (shift-expr add-expr (\? (or "<<<" ">>>") add-expr))
    (add-expr mul-expr (\? (char "+-") mul-expr))
    (mul-expr expon-expr (\? (char "*/%") expon-expr))
    (expon-expr unary-expr (\? "**" expon-expr)) ; right-associative
    :pass-thru nil
-   (unary-expr (or (: (or (char "!~+-") "void" "typeof" "delete") unary-expr)
+   (unary-expr (or (: (or (char "!~+-") `void `typeof `delete) unary-expr)
                    ;; TODO: next line needs to be lexical but member-expr needs to be syntactic
                    (: (or "--" "++") (* whitespace-no-eol) member-expr-with-call)
                    await
@@ -303,30 +303,30 @@
                          (\? ",")
                          (must-match ")" "msg.no.paren.arg"))))
    (member-expr-with-call :pass-thru t member-expr-head (* member-expr-tail-with-call))
-   (member-expr-head (or (: "new" member-expr (\? "\(" argument-list)) primary-expr))
+   (member-expr-head (or (: `new member-expr (\? "\(" argument-list)) primary-expr))
    (member-expr-tail-with-call (or (: "." property-access) (: "\[" element-get) function-call template-literal))
    (member-expr member-expr-head (* member-expr-tail))
    (member-expr-tail (or (: "." property-access) (: "\[" element-get) template-literal))
-   (primary-expr (or (: "class" class-expr)
-                     (: "function" function-expr)
-                     (: "async" "function" (with-context (async-p t) function-expr))
+   (primary-expr (or (: `class class-expr)
+                     (: `function function-expr)
+                     (: `async `function (with-context (async-p t) function-expr))
                      (: "\[" array-literal)
                      (: "{" object-literal)
-                     (: "let" let-expr)
+                     (: `let let-expr)
                      (: "\(" paren-expr)
                      keyword-lit name number string regexp
                      (: "..." name (\?= "\)" "=>")) ;; TODO: why?
                      reserved
                      (: eof (error "msg.unexpected.eof"))))
    (class-expr (\? name) class)
-   (class (\? "extends" (or (: (\?= "{") (error "msg.missing.extends"))
+   (class (\? `extends (or (: (\?= "{") (error "msg.missing.extends"))
                             assign-expr
                             (error "msg.bad.extends")))
           (must-match "{" "msg.no.brace.class")
           (* class-elem)
           (must-match "}" "msg.no.brace.prop"))
-   (class-elem (\? "static") (\? "*")
-               (\? (or "get" "set" "async") (= (or name string number "[")))
+   (class-elem (\? `static) (\? "*")
+               (\? (or `get `set `async) (= (or name string number "[")))
                (= (or name string number "["))
                named-class-prop
                (\? "," (error "msg.class.unexpected.comma"))
@@ -341,7 +341,7 @@
                          (with-context (star-p t) function))
                       (: (\? name) (must-match "(" "msg.no.paren.params")
                          (with-context (star-p nil) function))))
-   (async-function-expr "async" "function"
+   (async-function-expr `async `function
                         (or (: "*" (\? name) (must-match "(" "msg.no.paren.params")
                                (with-context (async-p t) (with-context (star-p t) function)))
                             (: (\? name) (must-match "(" "msg.no.paren.params")
@@ -352,7 +352,7 @@
                   (* ",")
                   ;; TODO: error if spread is not final arg in destructuring
                   (or "\]" (: eof (error "msg.no.bracket.arg"))))
-   (object-literal (* (or (: (or "*" "get" "set")
+   (object-literal (* (or (: (or "*" `get `set)
                              (or prop-name (error "msg.bad.prop"))
                              method-prop)
                           (: prop-name method-prop)
@@ -366,11 +366,17 @@
    (method-prop (must-match "\(" "msg.no.paren.parms") function)
    (plain-property (or (= (or "," "\}")) (must-match ":" "msg.no.colon.prop")))
    (paren-expr "\(" expr (must-match "\)" "msg.no.paren"))
-   (keyword-lit (or "null" "this" "super" "false" "true"))
-   (await "await" unary-expr)
+   (keyword-lit (or `null `this `super `false `true))
+   (await `await unary-expr)
    (property-access (must-match (or keyword reserved name) "msg.no.name.after.dot"))
    (element-get expr (must-match "\]" "msg.no.bracket.index"))
    (function-call "\(" argument-list)))
 
 (provide 'mole-js)
 ;;; mole-js ends here
+
+;; (font-lock-add-keywords nil mole-symbol-font-lock-kw t)
+
+;; Local Variables:
+;; mole-symbol-font-lock-kw: (("`\\sw+" . font-lock-constant-face))
+;; End:
